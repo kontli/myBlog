1bb23a86c14013a97e31ff39840ac5d2205539dc
lixj <lixj@gzviewit.com>
2019-08-06 11:38:04 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Test from '@/components/Test'
-import Love from '@/components/Love'
 
 Vue.use(Router)
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Test',
       component: Test
-    },
-    {
-      path: '/Love',
-      name: 'Love',
-      component: Love
     }
   ]
 })
